2a91275231585fea5f3e73be657de52d25889b62
willa <willa@indiefin.com>
2023-05-12 11:43:13 +0200

    fix: change getFrontendPath to use correct url function

--- backend/helpers/get-frontend-path.js
+++ backend/helpers/get-frontend-path.js
@@ -1,9 +1,9 @@
 import { dirname, join } from "path";
-import { pathToFileURL } from "url";
+import { fileURLToPath } from "url";
 
-export default getFrontendPath = () => {
-  const __filename = pathToFileURL(import.meta.url);
+export default function getFrontendPath() {
+  const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
   return join(__dirname.replace("/backend/helpers", ""), "/frontend");
-};
+}
