1e33273ae84a88cd1570dc56ca4b289279ab026c
willa <willa@indiefin.com>
2023-05-15 12:00:09 +0200

    feat: wrote newGame handlers, implemented them in bff, and added documentation for game and players

--- backend-for-frontend/bff.js
+++ backend-for-frontend/bff.js
@@ -4,20 +4,37 @@ import { getNewGame, postNewGame } from "./handlers/new-game.js";
 import { getPlayerJoin, postPlayerJoin } from "./handlers/player-join.js";
 import { getGameStart, postGameStart } from "./handlers/game-start.js";
 import express from "express";
+import { EventEmitter } from "events";
 
 const PORT = process.env.BFF_PORT || 3001;
 
-const bff = express().use(express.static("./frontend/"));
+const bff = express()
+  .use(express.static("./frontend/"))
+  .use(express.json());
 
+export const emitter = new EventEmitter();
+
+/**
+ * game.data should be of the following format:
+ * {
+ *   createdBy: <string> discord username of game creator
+ *   TODO: figure what else needs to be passed through
+ * }
+ */
 let game = {
   isReady: false,
   exists: false,
-  data: {}
-}
+  data: {},
+};
 
+/**
+ * a player {} in the array should be of the following format:
+ * {
+ *   username: <string> discord username
+ *   characterName: <string> name of character in the game
+ * }
+ */
 let players = [];
-
-bff.get("/", getSplash);
 
 bff.get("/new-game", (req, res) => getNewGame(req, res, game));
 bff.post("/new-game", (req, res) => postNewGame(req, res, game));
@@ -27,6 +44,8 @@ bff.post("/player-join", (req, res) => postPlayerJoin(req, res, players));
 
 bff.get("/game-start", getGameStart);
 bff.post("/game-start", postGameStart);
+
+bff.get("/", getSplash);
 
 bff.listen(PORT, () => {
   console.log(`bff started on port: ${PORT}`);
--- backend-for-frontend/handlers/new-game.js
+++ backend-for-frontend/handlers/new-game.js
@@ -1,7 +1,20 @@
-export const getNewGame = (req, res, game) => {
+import { emitter } from "../bff.js";
 
-}
+export const getNewGame = (req, res, game) => {
+  new Promise((resolve) => {
+    if (!game.exists) {
+      emitter.on("new-game", (gameData) => resolve(gameData));
+    } else {
+      resolve(game.data);
+    }
+  }).then((gameData) => {
+    res.status(200).json(gameData).end();
+  });
+};
 
 export const postNewGame = (req, res, game) => {
-
-}
+  game.data = req.body;
+  game.exists = true;
+  emitter.emit("new-game", game.data);
+  res.end("Game successfully posted");
+};
