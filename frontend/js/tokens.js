1d93c8faf2b217f791d425f3ae652b92b924948b
karishmag-bbd <122454780+karishmag-bbd@users.noreply.github.com>
2023-05-18 15:18:39 +0200

    finished the moving tokens

--- frontend/js/tokens.js
+++ frontend/js/tokens.js
@@ -1,42 +1,104 @@
-var container = document.getElementById('game');
-container.addEventListener('click', getClickPosition, false);
+let container = document.getElementById('game');
+let id;
+let offsets = {
+  red: {
+    x: document.getElementById('red').getBoundingClientRect().left,
+    y: document.getElementById('red').getBoundingClientRect().top,
+  },
+  green: {
+    x: document.getElementById('green').getBoundingClientRect().left,
+    y: document.getElementById('green').getBoundingClientRect().top,
+  },
+  blue: {
+    x: document.getElementById('blue').getBoundingClientRect().left,
+    y: document.getElementById('blue').getBoundingClientRect().top,
+  },
+  purple: {
+    x: document.getElementById('purple').getBoundingClientRect().left,
+    y: document.getElementById('purple').getBoundingClientRect().top,
+  },
+  white: {
+    x: document.getElementById('white').getBoundingClientRect().left,
+    y: document.getElementById('white').getBoundingClientRect().top,
+  },
+  yellow: {
+    x: document.getElementById('yellow').getBoundingClientRect().left,
+    y: document.getElementById('yellow').getBoundingClientRect().top,
+  },
+
+  candlestick: {
+    x: document.getElementById('candlestick').getBoundingClientRect().left,
+    y: document.getElementById('candlestick').getBoundingClientRect().top,
+  },
+  knife: {
+    x: document.getElementById('knife').getBoundingClientRect().left,
+    y: document.getElementById('knife').getBoundingClientRect().top,
+  },
+  leadpipe: {
+    x: document.getElementById('leadpipe').getBoundingClientRect().left,
+    y: document.getElementById('leadpipe').getBoundingClientRect().top,
+  },
+  revolver: {
+    x: document.getElementById('revolver').getBoundingClientRect().left,
+    y: document.getElementById('revolver').getBoundingClientRect().top,
+  },
+  wrench: {
+    x: document.getElementById('wrench').getBoundingClientRect().left,
+    y: document.getElementById('wrench').getBoundingClientRect().top,
+  },
+  rope: {
+    x: document.getElementById('rope').getBoundingClientRect().left,
+    y: document.getElementById('rope').getBoundingClientRect().top,
+  },
+  gun: {
+    x: document.getElementById('revolver').getBoundingClientRect().left,
+    y: document.getElementById('revolver').getBoundingClientRect().top,
+  },
+};
+
+container.addEventListener('click', getClickPosition);
 
 function getClickPosition(e) {
-  console.log(e.target.id);
+  if (e.target.id != 'board') {
+    id = e.target.id;
+    clickItem = document.getElementById(id);
+    console.log(
+      clickItem.getBoundingClientRect().top,
+      clickItem.getBoundingClientRect().left
+    );
+  } else {
+    let parentPosition = getPosition(container);
+    let xPosition = e.clientX - parentPosition.x - clickItem.clientWidth / 2;
+
+    let yPosition = e.clientY - parentPosition.y - clickItem.clientHeight / 2;
+
+    xPosition -= offsets[id].x;
+    yPosition -= offsets[id].y;
+    clickItem.style.left = xPosition + 'px';
+    clickItem.style.top = yPosition + 'px';
+  }
 }
 
-// function getClickPosition(e) {
-//   var parentPosition = getPosition(e.currentTarget);
-//   var xPosition = e.clientX - parentPosition.x - theThing.clientWidth / 2;
-//   var yPosition = e.clientY - parentPosition.y - theThing.clientHeight / 2;
+function getPosition(el) {
+  let xPos = 0;
+  let yPos = 0;
 
-//   theThing.style.left = xPosition + 'px';
-//   theThing.style.top = yPosition + 'px';
-// }
+  while (el) {
+    if (el.tagName == 'ARTICLE') {
+      let xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      let yScroll = el.scrollTop || document.documentElement.scrollTop;
 
-// // Helper function to get an element's exact position
-// function getPosition(el) {
-//   var xPos = 0;
-//   var yPos = 0;
+      xPos = el.offsetLeft - xScroll + el.clientLeft;
+      yPos = el.offsetTop - yScroll + el.clientTop;
+    } else {
+      xPos = el.offsetLeft - el.scrollLeft + el.clientLeft;
+      yPos = el.offsetTop - el.scrollTop + el.clientTop;
+    }
 
-//   while (el) {
-//     if (el.tagName == 'BODY') {
-//       // deal with browser quirks with body/window/document and page scroll
-//       var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-//       var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-//       xPos += el.offsetLeft - xScroll + el.clientLeft;
-//       yPos += el.offsetTop - yScroll + el.clientTop;
-//     } else {
-//       // for all other non-BODY elements
-//       xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
-//       yPos += el.offsetTop - el.scrollTop + el.clientTop;
-//     }
-
-//     el = el.offsetParent;
-//   }
-//   return {
-//     x: xPos,
-//     y: yPos,
-//   };
-// }
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos,
+  };
+}
