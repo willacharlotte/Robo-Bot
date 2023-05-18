d0b60f82874f8a066eafb6cff50ad4986ac40fe2
DanielSBBD <daniels@bbd.co.za>
2023-05-18 11:04:45 +0200

    fix: actual fix for file path bug'

--- backend-for-frontend/handlers/splash.js
+++ backend-for-frontend/handlers/splash.js
@@ -2,6 +2,6 @@ import { join } from 'path';
 import { root } from '../bff.js';
 
 export default function getSplash(_, res) {
-  const htmlFile = join('../..', '/html/index.html');
+  const htmlFile = 'frontend/html/index.html';
   res.sendFile(htmlFile, { root: root });
 }
