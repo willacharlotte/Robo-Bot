49946cd87d4a4f69eeae8969f7bc8dfdbed51898
karishmag-bbd <122454780+karishmag-bbd@users.noreply.github.com>
2023-05-22 17:44:52 +0200

    I cri

--- frontend/js/splash.js
+++ frontend/js/splash.js
@@ -1,39 +1,15 @@
 const hide = (element) => {
-<<<<<<< HEAD
-  element.classList.add("hide");
-};
-
-const show = (element) => {
-  element.classList.remove("hide");
-=======
   element.classList.add('hide');
 };
 
 const show = (element) => {
   element.classList.remove('hide');
->>>>>>> main
 };
 
 const bindData = (element, key, value) => {
   element.innerHTML = element.innerHTML.replace(`{{${key}}}`, value);
 };
 
-<<<<<<< HEAD
-const url = "http://localhost:3001";
-
-const awaitNewGame = document.getElementById("await-new-game");
-const newGame = document.getElementById("new-game");
-const awaitPlayers = document.getElementById("await-players");
-const playersTable = document.getElementById("players-table")
-const playersTableBody = document.getElementById("players-table-body");
-const playerTemplate = document.getElementById("player-template");
-const awaitGameStart = document.getElementById("await-game-start");
-const gameStart = document.getElementById("game-start");
-
-async function waitingRoom() {
-  const gameData = await fetch(url + "/new-game").then((res) => res.json());
-  bindData(newGame, "createdBy", gameData.createdBy);
-=======
 const url = 'http://localhost:3001';
 
 const awaitNewGame = document.getElementById('await-new-game');
@@ -59,7 +35,6 @@ async function startGameOnClick() {
 async function waitingRoom() {
   const gameData = await fetch(url + '/new-game').then((res) => res.json());
   bindData(newGame, 'createdBy', gameData.createdBy);
->>>>>>> main
 
   hide(awaitNewGame);
   show(newGame);
@@ -67,41 +42,27 @@ async function waitingRoom() {
   const players = [];
   show(awaitPlayers);
   show(playersTable);
-<<<<<<< HEAD
-
-  while (players.length < 6) {
-    const newPlayerData = await fetch(url + "/player-join").then((res) =>
-=======
   show(awaitGameStart);
 
   while (players.length < 6) {
     const newPlayerData = await fetch(url + '/player-join').then((res) =>
->>>>>>> main
       res.json()
     );
     const newPlayerNode = playerTemplate.cloneNode(true);
 
-<<<<<<< HEAD
-    bindData(newPlayerNode, "username", newPlayerData.username);
-    bindData(newPlayerNode, "characterName", newPlayerData.characterName);
-=======
     bindData(newPlayerNode, 'username', newPlayerData.username);
     bindData(newPlayerNode, 'characterName', newPlayerData.characterName);
->>>>>>> main
 
     playersTableBody.appendChild(newPlayerNode);
     show(newPlayerNode);
 
     players.push(newPlayerData);
   }
-<<<<<<< HEAD
-=======
   show(startGame);
   hide(awaitGameStart);
 
   await startGameOnClick();
   window.location.href = `/game/${gameData.id}`;
->>>>>>> main
 }
 
 waitingRoom();
