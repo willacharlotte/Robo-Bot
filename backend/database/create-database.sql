e231dacb836566677466a4086c52f0fc1b1a2a94
DanielSBBD <daniels@bbd.co.za>
2023-05-24 21:45:20 +0200

    Implemented db

---
+++ backend/database/create-database.sql
@@ -0,0 +1,56 @@
+CREATE TABLE `game_object_types` (
+  `type_id` integer AUTO_INCREMENT,
+  `type_name` varchar(255),
+  PRIMARY KEY (type_id)
+);
+
+CREATE TABLE `game_objects` (
+  `object_id` integer AUTO_INCREMENT,
+  `object_type_id` integer,
+  `object_type_name` varchar(255),
+  `object_description` varchar(255),
+  PRIMARY KEY (object_id)
+);
+
+CREATE TABLE `player_games` (
+  `player_game_id` integer AUTO_INCREMENT,
+  `player_id` integer,
+  `game_id` integer,
+  `character_id` integer,
+  PRIMARY KEY (player_game_id)
+);
+
+CREATE TABLE `players` (
+  `player_id` integer AUTO_INCREMENT,
+  `player_username` varchar(255),
+  PRIMARY KEY (player_id)
+);
+
+CREATE TABLE `game_cards` (
+  `player_game_id` integer,
+  `game_object_id` integer
+);
+
+CREATE TABLE `case_file_confidential` (
+  `game_id` integer AUTO_INCREMENT,
+  `suspect_id` integer,
+  `weapon_id` integer,
+  `room_id` integer,
+  PRIMARY KEY (game_id)
+);
+
+ALTER TABLE `game_objects` ADD FOREIGN KEY (`object_type_id`) REFERENCES `game_object_types` (`type_id`);
+
+ALTER TABLE `player_games` ADD FOREIGN KEY (`player_id`) REFERENCES `players` (`player_id`);
+
+ALTER TABLE `player_games` ADD FOREIGN KEY (`character_id`) REFERENCES `game_objects` (`object_id`);
+
+ALTER TABLE `game_cards` ADD FOREIGN KEY (`player_game_id`) REFERENCES `player_games` (`player_game_id`);
+
+ALTER TABLE `game_cards` ADD FOREIGN KEY (`game_object_id`) REFERENCES `game_objects` (`object_id`);
+
+ALTER TABLE `case_file_confidential` ADD FOREIGN KEY (`suspect_id`) REFERENCES `game_objects` (`object_id`);
+
+ALTER TABLE `case_file_confidential` ADD FOREIGN KEY (`weapon_id`) REFERENCES `game_objects` (`object_id`);
+
+ALTER TABLE `case_file_confidential` ADD FOREIGN KEY (`room_id`) REFERENCES `game_objects` (`object_id`);
---
+++ backend/database/populate-database.sql
@@ -0,0 +1,29 @@
+INSERT INTO game_object_types (type_name)
+VALUES
+('SUSPECT'),
+('WEAPON'),
+('ROOM');
+
+INSERT INTO game_objects (object_type_id, object_description)
+VALUES
+(1, 'Colonel Mustard'),
+(1, 'Miss Scarlet'),
+(1, 'Professor Plum'),
+(1, 'Mr. Green'),
+(1, 'Mrs. White'),
+(1, 'Mrs. Peacock'),
+(2, 'Rope'),
+(2, 'Lead Pipe'),
+(2, 'Knife'),
+(2, 'Wrench'),
+(2, 'Candlestick'),
+(2, 'Revolver'),
+(3, 'Hall'),
+(3, 'Study'),
+(3, 'Library'),
+(3, 'Billiard Room'),
+(3, 'Conservatory'),
+(3, 'Ball Room'),
+(3, 'Kitchen'),
+(3, 'Dining Room'),
+(3, 'Lounge');
