4250d077cacf7742eca9da2637cb110ec87fb2a1
DanielSBBD <daniels@bbd.co.za>
2023-05-25 22:24:04 +0200

    Made frontend prettier - still needs to be made functional again lmao

--- frontend/js/splash.js
+++ frontend/js/splash.js
@@ -1,47 +1,29 @@
 const hide = (element) => {
-  element.classList.add('hide');
+  element.classList.add("hide");
 };
 
 const show = (element) => {
-  element.classList.remove('hide');
+  element.classList.remove("hide");
 };
 
 const bindData = (element, key, value) => {
   element.innerHTML = element.innerHTML.replace(`{{${key}}}`, value);
 };
 
-const url = 'http://localhost:3001';
+const url = "http://localhost:3001";
 
-const awaitNewGame = document.getElementById('await-new-game');
-const newGame = document.getElementById('new-game');
-const awaitPlayers = document.getElementById('await-players');
-const playersTable = document.getElementById('players-table');
-const playersTableBody = document.getElementById('players-table-body');
-const playerTemplate = document.getElementById('player-template');
-const awaitGameStart = document.getElementById('await-game-start');
-const startGame = document.getElementById('start-game');
-const gameStarted = document.getElementById('game-started');
-
-async function startGameOnClick(gameData) {
-  show(startGame);
-  hide(awaitGameStart);
-
-  startGame.addEventListener('click', async (event) => {
-    event.preventDefault();
-    show(gameStarted);
-    const options = {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-      body: JSON.stringify({ id: gameData.id }),
-    };
-    await fetch(url + '/game-start', options);
-    window.location.href = `/game/${gameData.id}`;
-  });
-}
+const awaitNewGame = document.getElementById("await-new-game");
+const newGame = document.getElementById("new-game");
+const awaitPlayers = document.getElementById("await-players");
+const playersTable = document.getElementById("players-table")
+const playersTableBody = document.getElementById("players-table-body");
+const playerTemplate = document.getElementById("player-template");
+const awaitGameStart = document.getElementById("await-game-start");
+const gameStart = document.getElementById("game-start");
 
 async function waitingRoom() {
-  const gameData = await fetch(url + '/new-game').then((res) => res.json());
-  bindData(newGame, 'createdBy', gameData.createdBy);
+  const gameData = await fetch(url + "/new-game").then((res) => res.json());
+  bindData(newGame, "createdBy", gameData.createdBy);
 
   hide(awaitNewGame);
   show(newGame);
@@ -49,20 +31,15 @@ async function waitingRoom() {
   const players = [];
   show(awaitPlayers);
   show(playersTable);
-  show(awaitGameStart);
 
   while (players.length < 6) {
-    if (players.length === 2) {
-      startGameOnClick(gameData);
-    }
-
-    const newPlayerData = await fetch(url + '/player-join').then((res) =>
+    const newPlayerData = await fetch(url + "/player-join").then((res) =>
       res.json()
     );
     const newPlayerNode = playerTemplate.cloneNode(true);
 
-    bindData(newPlayerNode, 'username', newPlayerData.username);
-    bindData(newPlayerNode, 'characterName', newPlayerData.characterName);
+    bindData(newPlayerNode, "username", newPlayerData.username);
+    bindData(newPlayerNode, "characterName", newPlayerData.characterName);
 
     playersTableBody.appendChild(newPlayerNode);
     show(newPlayerNode);
